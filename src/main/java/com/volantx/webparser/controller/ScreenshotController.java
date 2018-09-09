a6420658da9c28a5db2cd783733b37b307928a5f
Kamuran <ieckamuran@gmail.com>
2018-09-09 20:08:00 +0300

    controller

--- src/main/java/com/volantx/webparser/controller/ScreenshotController.java
+++ src/main/java/com/volantx/webparser/controller/ScreenshotController.java
@@ -1,26 +1,25 @@
 package com.volantx.webparser.controller;
 
 
-import org.springframework.kafka.test.rule.KafkaEmbedded;
+import com.volantx.webparser.service.SnapshotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class ScreenshotController {
 
-    private static final String TOPIC_NAME = "smapshotTopic";
+    @Autowired
+    private SnapshotService snapshotService;
 
-    private KafkaEmbedded kafkaEmbedded;
-
-    @RequestMapping("/api/takeSnapshot")
-    @ResponseBody String takeSnapshot(@RequestBody List<String> urls) {
-        kafkaEmbedded = new KafkaEmbedded(1, true, TOPIC_NAME);
-        return null;
+    @RequestMapping(value = "/api/takeSnapshot", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody int takeSnapshot(@RequestBody List<String> urls) {
+        return snapshotService.produceSnapshotTasks(urls);
     }
 
 }
--- src/main/java/com/volantx/webparser/service/SnapshotService.java
+++ src/main/java/com/volantx/webparser/service/SnapshotService.java
@@ -1,22 +1,35 @@
 package com.volantx.webparser.service;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class SnapshotService {
 
+    private static final String TOPIC_NAME = "snapshotTopic";
+
+    private KafkaEmbedded kafkaEmbedded;
 
     @Value("${selenium.webdriver.chrome}")
     private String chromeWebDriver;
@@ -30,12 +43,16 @@ public class SnapshotService {
     public WebDriver driver;
     private File savePathDir;
 
+    Logger logger = LoggerFactory.getLogger(SnapshotService.class);
 
     @PostConstruct
-    void init()  {
+    void init() {
 
-        System.setProperty("webdriver.chrome.driver",chromeWebDriver);
-        savePathDir  = new File(savePath);
+        kafkaEmbedded = new KafkaEmbedded(1, true, TOPIC_NAME);
+
+        System.setProperty("webdriver.chrome.driver", chromeWebDriver);
+
+        savePathDir = new File(savePath);
         setupTest();
         try {
             screenshotGetScreenShotAs();
@@ -58,9 +75,25 @@ public class SnapshotService {
 
     public void screenshotGetScreenShotAs() throws IOException {
         //Take Screenshot of viewable area
-        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         //Write Screenshot to a file
-        FileUtils.copyFile(scrFile, File.createTempFile("snap",".png", savePathDir));
+        FileUtils.copyFile(scrFile, File.createTempFile("snap", ".png", savePathDir));
+    }
+
+    public int produceSnapshotTasks(List<String> urls) {
+        Map<String, Object> senderProps = KafkaTestUtils.producerProps(kafkaEmbedded);
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(senderProps);
+
+        AtomicInteger accepted=new AtomicInteger();
+        urls.stream().forEach(url -> {
+            try {
+                producer.send(new ProducerRecord<>(TOPIC_NAME, 0, 0, url)).get();
+                accepted.incrementAndGet();
+            } catch (Exception e) {
+                logger.error("Cannot add url:" + url, e);
+            }
+        });
+        return accepted.get();
     }
 
 }
