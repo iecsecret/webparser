84f73ce9572c37d1c3748d135464d6611c08fe54
Kamuran <ieckamuran@gmail.com>
2018-09-09 21:53:56 +0300

    controller

---
+++ src/main/java/com/volantx/webparser/service/SnapshotConsumer.java
@@ -0,0 +1,61 @@
+package com.volantx.webparser.service;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+public class SnapshotConsumer implements Runnable {
+
+    private KafkaConsumer<String, String> kafkaConsumer;
+
+    private File savePathDir;
+
+    Logger logger = LoggerFactory.getLogger(SnapshotConsumer.class);
+
+    private int index;
+
+    public SnapshotConsumer(Map<String, Object> props, String saveUrl, String savePath, int index) {
+
+        this.index = index;
+
+        savePathDir = new File(savePath);
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        kafkaConsumer = new KafkaConsumer<>(props);
+        kafkaConsumer.subscribe(Arrays.asList(new String[]{SnapshotService.TOPIC_NAME}));
+
+    }
+
+    @Override
+    public void run() {
+        ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Integer.MAX_VALUE);
+        consumerRecords.records(SnapshotService.TOPIC_NAME).forEach(record -> {
+            String url = record.value();
+            WebDriver chromeDriver = SnapshotService.chromeDrivers.get(index);
+            chromeDriver.navigate().to(record.value());
+            chromeDriver.manage().window().maximize();
+            File scrFile = ((TakesScreenshot) chromeDriver).getScreenshotAs(OutputType.FILE);
+
+            try {
+                FileUtils.copyFile(scrFile, File.createTempFile("snap", ".png", savePathDir));
+            } catch (IOException e) {
+                logger.error("Createsnapshot failed for url:" + url);
+            }
+        });
+    }
+}
--- src/main/java/com/volantx/webparser/service/SnapshotService.java
+++ src/main/java/com/volantx/webparser/service/SnapshotService.java
@@ -1,13 +1,18 @@
 package com.volantx.webparser.service;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.mobile.NetworkConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,18 +23,23 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class SnapshotService {
 
-    private static final String TOPIC_NAME = "snapshotTopic";
+    public static ArrayList<ChromeDriver> chromeDrivers = new ArrayList<>();
+
+    public static final String TOPIC_NAME = "snapshotTopic";
 
     private KafkaEmbedded kafkaEmbedded;
+
+    @Value("${kafka.consumer.count:1}")
+    private int kafkaConsumerCount;
 
     @Value("${selenium.webdriver.chrome}")
     private String chromeWebDriver;
@@ -40,51 +50,41 @@ public class SnapshotService {
     @Value("${snap.save.url}")
     private String saveUrl;
 
-    public WebDriver driver;
-    private File savePathDir;
-
     Logger logger = LoggerFactory.getLogger(SnapshotService.class);
+
 
     @PostConstruct
     void init() {
 
-        kafkaEmbedded = new KafkaEmbedded(1, true, TOPIC_NAME);
-
         System.setProperty("webdriver.chrome.driver", chromeWebDriver);
 
-        savePathDir = new File(savePath);
-        setupTest();
-        try {
-            screenshotGetScreenShotAs();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 1; i <= kafkaConsumerCount; i++) {
+
+            ChromeOptions ops = new ChromeOptions();
+            ops.addArguments("--headless");
+            ChromeDriverService chromeDriverService = new ChromeDriverService.Builder().withVerbose(true).withWhitelistedIps("").build();
+            ChromeDriver chromeDriver = new ChromeDriver(chromeDriverService, ops);
+            chromeDrivers.add(chromeDriver);
         }
-    }
 
 
-    //Setup Driver
-    public void setupTest() {
-        ChromeOptions ops = new ChromeOptions();
-        ops.addArguments("--whitelisted-ips=''");
-        ops.addArguments("--headless");
-        driver = new ChromeDriver(ops);
-        //Navigate to URL
-        driver.navigate().to(saveUrl);
-        driver.manage().window().maximize();
-    }
+        kafkaEmbedded = new KafkaEmbedded(1, true, TOPIC_NAME);
 
-    public void screenshotGetScreenShotAs() throws IOException {
-        //Take Screenshot of viewable area
-        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        //Write Screenshot to a file
-        FileUtils.copyFile(scrFile, File.createTempFile("snap", ".png", savePathDir));
+        Map<String, Object> props = KafkaTestUtils.producerProps(kafkaEmbedded);
+
+        ExecutorService executor = Executors.newFixedThreadPool(kafkaConsumerCount);
+
+        for (int i = 1; i <= kafkaConsumerCount; i++) {
+            executor.submit(new SnapshotConsumer(props, saveUrl, savePath, i));
+        }
+
     }
 
     public int produceSnapshotTasks(List<String> urls) {
         Map<String, Object> senderProps = KafkaTestUtils.producerProps(kafkaEmbedded);
         KafkaProducer<Integer, String> producer = new KafkaProducer<>(senderProps);
 
-        AtomicInteger accepted=new AtomicInteger();
+        AtomicInteger accepted = new AtomicInteger();
         urls.stream().forEach(url -> {
             try {
                 producer.send(new ProducerRecord<>(TOPIC_NAME, 0, 0, url)).get();
@@ -95,5 +95,6 @@ public class SnapshotService {
         });
         return accepted.get();
     }
+
 
 }
